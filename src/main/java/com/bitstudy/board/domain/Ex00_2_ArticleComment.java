9868f9add3a409784f6f51fb9df8c0f95cf817cd
0hangyeol0 <145204468+0hangyeol0@users.noreply.github.com>
2024-06-13 12:24:20 +0900

    #11 - 도메인 코드를 앤티티로 변경
    1 도메인 = 1 테이블로 매칭되도록 변환
    (각 클래스 위에 @Entity 넣음)
    생성자, equals(), hashcode() 삽입

--- src/main/java/com/bitstudy/board/domain/Ex00_2_ArticleComment.java
+++ src/main/java/com/bitstudy/board/domain/Ex00_2_ArticleComment.java
@@ -2,10 +2,10 @@ package com.bitstudy.board.domain;
 
 import java.time.LocalDateTime;
 
-public class AriticleComment {
+public class Ex00_2_ArticleComment {
     private Long id; // 고유번호
 
-    private Ariticle article; // 연관관계 매핑.
+    private Ex00_1_Article article; // 연관관계 매핑.
     /* 연관 관계 없이 코드를 짤거라면
     * private Long FK_id 이런식으로
     * Article과 관계를 맺고 있는 필드 라는걸 객체지향적으로 표현한거임 */
